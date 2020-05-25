096764c787db6d157d16885603ae546f0ddc854f
Siddhi Gupta <vrindagupta07@gmail.com>
2020-05-25 23:40:52 +0530

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models/user';
+import { AuthenticationService } from '../_services/authentication.service';
+import { Router } from '@angular/router';
+
+@Component(
+    { 
+        templateUrl: 'home.component.html',
+        styleUrls: ['./home.component.scss']
+    }
+    )
+export class HomeComponent {
+
+    constructor(private authenticationService: AuthenticationService, private router:Router) { }
+
+    ngOnInit() {
+        
+    }
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
+}
